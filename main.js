f3a3313af79f600388951de4cec9743b97f9e58f
Ксения Блинкова <bks2607@yandex.ru>
2026-05-05 21:39:32 +0300

    update index.html

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 import { MainPage } from "./pages/main/main.js";
 import { ButtonHome } from "./components/button-home/button-home.js";
+// Импортируем стили (Vite сам найдет их в node_modules или в папке проекта)
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css'; 
+
+// Импортируем скрипты библиотек
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = document.getElementById('root');
 const headerAction = document.getElementById('home-button-container');
